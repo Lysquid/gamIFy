36e64a1585665b58986374facaedeabcfd04e5a9
Gartox <ewan71230@gmail.com>
2023-12-06 09:34:36 +0100

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="bg-gradient-to-r from-cyan-500 to-emerald-500">Welcome to SvelteKit</h1>
+<h2>Hello</h2>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
