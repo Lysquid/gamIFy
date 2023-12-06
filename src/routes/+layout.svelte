2ed738ba05f1c1235372007f66f6203a9d037fea
Alix Peigue <irobot.moi@gmail.com>
2023-12-06 13:24:30 +0100

    home page with static content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>import "../app.pcss";</script>
+
+<div class="bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-white">
+    <h1 class="bg-gradient-to-r from-cyan-500 to-emerald-500 text-center p-5 text-3xl text-white font-bold">GamIFy</h1>
+    <slot></slot>
+    <footer class="mt-10">
+        <p>This is a footer</p>
+    </footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,62 @@
-<h1 class="bg-gradient-to-r from-cyan-500 to-emerald-500">Welcome to SvelteKit</h1>
-<h2>Hello</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let data = {
+        result : [
+            {
+                title : "Super Mario",
+                publisher: "Nintendo",
+                image : "https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.hURFJf6p7dZYlpAZScKGDAHaKI%26pid%3DApi&sp=1701857939T2f192bc62320b3bade779ffd47ece0d596f4e34846a5d6f8b6b9aadfee481c02",
+            },
+
+        ],
+    };
+</script>
+
+
+<p class="text-gray-900 dark:text-white text-center text-2xl my-10">The ultimate Video Game search engine !</p>
+<form>
+    <div class="flex max-w-3xl m-auto">
+        <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
+        <button id="dropdown-button" data-dropdown-toggle="dropdown" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">
+            All categories 
+            <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/></svg>
+        </button>
+        <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+            <li>
+                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mockups</button>
+            </li>
+            <li>
+                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Templates</button>
+            </li>
+            <li>
+                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Design</button>
+            </li>
+            <li>
+                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logos</button>
+            </li>
+            </ul>
+        </div>
+        <div class="relative w-full">
+            <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required>
+            <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+                <span class="sr-only">Search</span>
+            </button>
+        </div>
+    </div>
+</form>
+
+<div class="flex flex-col mx-50 gap-5 mt-10 items-center">
+    {#each data.result as item}
+    <div class="flex bg-white hover:bg-gray-50 shadow-lg dark:bg-gray-700 dark:hover:bg-gray-600 h-32 gap-5 rounded-xl overflow-hidden hover:scale-105 transition cursor-pointer">
+        <img class="w-32 object-cover" src="{item.image}" alt=""/>
+        <div class="my-auto w-96">
+            <h1 class="text-xl">{item.title}</h1>
+            <p class="dark:text-gray-400 text-gray-600">published by <strong>{item.publisher}</strong></p>
+        </div>
+    </div>
+    {/each}
+</div>
+
