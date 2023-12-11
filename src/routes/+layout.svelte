5211d255baf7aa4438872c9c58a3bf8cd83b84b2
Gartox <ewan71230@gmail.com>
2023-12-11 10:45:19 +0100

    Layout update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>import "../app.pcss";</script>
 
-<div class="bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-white">
+<div class="bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-white min-h-screen flex flex-col justify-between">
     <h1 class="bg-gradient-to-r from-cyan-500 to-emerald-500 text-center p-5 text-3xl text-white font-bold">GamIFy</h1>
-    <slot></slot>
-    <footer class="mt-10">
-        <p>This is a footer</p>
+    <div class="mb-auto">
+        <slot></slot>
+    </div>
+    <footer class="mt-10 bg-gradient-to-r from-cyan-500 to-emerald-500">
+        <p class="p-1 text-1xl text-white text-right pr-3">Developped by <strong>Blazanome©</strong></p>
     </footer>
 </div>
