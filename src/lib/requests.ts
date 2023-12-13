4cf3f2a5fe11f0a1959dd4dfefae6b30aaa1b988
Toto <theo.chone@gmail.com>
2023-12-13 09:33:42 +0100

    Publisher page + ratio

--- src/lib/requests.ts
+++ src/lib/requests.ts
@@ -80,16 +80,19 @@ export async function searchGamePlatforms(game: string): Promise<any> {
 export async function searchPublisherInfo(publisher: string): Promise<any> {
 
     let query = `
-        SELECT ?publisherlabel ?description ?emp ?homepage ?image WHERE {
+        SELECT ?publisherlabel ?description ?emp ?homepage ?image ?citylabel GROUP_CONCAT(DISTINCT ?people; SEPARATOR=" | ") as ?keyPeople WHERE {
         BIND(<http://dbpedia.org/resource/${publisher}> AS ?publisher).
         ?publisher rdfs:label ?publisherlabel.
         ?publisher ^dbo:publisher ?game.
         FILTER(lang(?publisherlabel) = "en").
         OPTIONAL {?publisher dbo:abstract ?description. FILTER(lang(?description) = "en")}.
-        OPTIONAL {?publisher dbo:numberOfEmployees ?emp.}
+        OPTIONAL {?publisher dbo:numberOfEmployees ?emp.}.
         OPTIONAL {?publisher foaf:homepage ?homepage.}.
         OPTIONAL {?publisher dbo:thumbnail ?image.}.
+        OPTIONAL {?publisher dbo:locationCity ?city. ?city rdfs:label ?citylabel. FILTER(lang(?citylabel) = "en").}.
+        ?publisher dbp:keyPeople ?people.
         }
+        GROUP BY ?publisherlabel ?description ?emp ?homepage ?image ?citylabel
         LIMIT 1
     `
     return executeQuery(query);
--- src/routes/publisher/[slug]/+page.svelte
+++ src/routes/publisher/[slug]/+page.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
 	import { searchImage, searchPublisherInfo } from '$lib/requests';
-	import { Spinner } from 'flowbite-svelte';
+	import { Spinner, A } from 'flowbite-svelte';
 
 	export let data: PageData;
 	let loading = true;
@@ -26,12 +26,20 @@
 		<Spinner></Spinner>
 	{:else if publisher_data}
 		<div class="flex flex-row justify-between space-x-10">
-			<div class="mr-auto w-3/4">
+			<div class="mr-auto w-1/2">
 				<h1 class="text-bold text-5xl mb-10">{publisher_data?.publisherlabel?.value}</h1>
-                <p class="text-justify">{publisher_data?.description?.value || ''}</p>
+                <p class="text-justify mb-10">{publisher_data?.description?.value || ''}</p>
+				<h1 class="text-bold text-2xl">{'Number of employees'}</h1>
+				<p class="text-justify mb-5">{publisher_data?.emp?.value}</p>
+				<h1 class="text-bold text-2xl">{'Website of the company'}</h1>
+				<A class="text-justify mb-5" href={publisher_data?.homepage?.value}>{publisher_data?.homepage?.value}</A>
+				<h1 class="text-bold text-2xl">{'Key people'}</h1>
+				<p class="text-justify mb-5">{publisher_data?.keyPeople?.value}</p>
+				<h1 class="text-bold text-2xl">{'Headquarters'}</h1>
+				<p class="text-justify mb-5">{publisher_data?.citylabel?.value}</p>
 			</div>
 			{#if publisher_data.image}
-				<img class="w-1/4" src={publisher_data.image} alt="Publisher logo" />
+				<img class="w-1/2" src={publisher_data.image} alt="Publisher logo" />
 			{/if}
 		</div>
 	{:else}
