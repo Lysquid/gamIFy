55e8448e7b153e18d9a58cc7b6d860e0123223aa
Lysquid <romainbenoit21560@gmail.com>
2023-12-13 09:11:12 +0100

    list item

---
+++ src/lib/components/ListBox.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+    export var name:string;
+    export var url:string;
+    export var image:string = '';
+    export var description:string = '';
+
+</script>
+
+<a href="/game/{encodeURIComponent(url.split('/').slice(-1))}" class="flex bg-white hover:bg-gray-50 shadow-lg dark:bg-gray-700 dark:hover:bg-gray-600 h-32 gap-5 rounded-xl overflow-hidden hover:scale-105 transition cursor-pointer">
+    {#if image}
+    <img class="w-32 object-contain" src="{image}" alt=""/>
+    {:else}
+    <div class="w-32 object-contain"></div>
+    {/if}
+    <div class="my-auto w-96">
+        <h1 class="text-xl">{name}</h1>
+        {#if description}
+            <p class="dark:text-gray-400 text-gray-600">published by <strong>{description}</strong></p>
+        {/if}
+    </div>
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { ValueContainsFilter, searchGames, searchImage } from '$lib/requests';
+	import ListBox from '$lib/components/ListBox.svelte';
 	import { Button, Dropdown, DropdownItem, Spinner } from 'flowbite-svelte';
 
 	let search = '';
@@ -158,7 +159,8 @@
                     {/if}
                 </div>
             </a>
-        {/each}
+			<!-- <ListBox url={item.game.value} image={item.image.value} name={item.gamelabel.value} description={item.publisherlabel.value}></ListBox> -->
+		{/each}
     </div>
 {:else if data}
     <p class="text-center m-20 text-lg dark:text-gray-400 text-gray-600">No results</p>
