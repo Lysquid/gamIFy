90e09a25e8686e76ecdf7b8af211eed1e3ebad91
Alix Peigue <irobot.moi@gmail.com>
2023-12-13 22:31:13 +0100

    header redirects to search page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>import "../app.pcss";</script>
 
 <div class="bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-white min-h-screen flex flex-col justify-between">
-    <h1 class="bg-gradient-to-r from-cyan-500 to-emerald-500 text-center p-5 text-3xl text-white font-bold">GamIFy</h1>
+    <a class="bg-gradient-to-r from-cyan-500 to-emerald-500 text-center p-5 text-3xl text-white font-bold" href="/">GamIFy</a>
     <div class="mb-auto">
         <slot></slot>
     </div>
