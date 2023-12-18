7c4016d0749d0b6fd636c14ebe07ecb12910b084
Alix Peigue <irobot.moi@gmail.com>
2023-12-18 12:37:49 +0100

    fix des problèmes qui existaient pas avant le refactor

--- src/lib/components/ListBox.svelte
+++ src/lib/components/ListBox.svelte
@@ -19,21 +19,19 @@
     export var uri:string;
     export var image:string|undefined;
 
-    onMount(async () => {
-        if (image) {
-            image = await searchImage(image);
-        }
-	});
-
 </script>
 
 
 <a href="/{type}/{encodeURIComponent(uri.split('/').slice(-1)[0])}" class="flex bg-white hover:bg-gray-50 shadow-lg dark:bg-gray-700 dark:hover:bg-gray-600 h-32 gap-5 rounded-xl overflow-hidden hover:scale-105 transition cursor-pointer">
-    {#if image}
-        <img class="w-32 object-contain" src="{image}" alt=""/>
-    {:else}
+    {#await searchImage(image || "")}
         <div class="w-32 object-contain"></div>
-    {/if}
+    {:then image}
+        {#if image}
+            <img class="w-32 object-contain" src="{image}" alt=""/>
+        {:else}
+            <div class="w-32 object-contain"></div>
+        {/if}
+    {/await}
     <div class="my-auto w-96">
         <h1 class="text-xl">{title}</h1>
         {#if description}
